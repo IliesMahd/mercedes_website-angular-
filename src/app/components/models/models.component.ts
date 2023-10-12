254f7394fd9502e3d1bc68b3b0d66d195c044d8b
boubouuubinks <43859957+Neqoo@users.noreply.github.com>
2023-10-12 23:40:45 +0200

    création component svg

--- src/app/components/models/models.component.ts
+++ src/app/components/models/models.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 interface Brand {
   name: string;
   logo?: string;
   label?: string;
   icon?: string;
+  sanitizedLogo?: SafeResourceUrl;
 }
 
 @Component({
@@ -13,24 +15,36 @@ interface Brand {
   styleUrls: ['./models.component.scss'],
 })
 export class ModelsComponent {
+  constructor(
+    private sanitizer: DomSanitizer,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
   private brands: Brand[] = [
     { name: 'all', label: 'Tous les modèles' },
-    { name: 'amg', logo: 'amg.svg' },
-    { name: 'maybach', logo: 'maybach.svg' },
+    { name: 'amg', logo: '/assets/brands/amg.svg' },
+    { name: 'maybach', logo: '/assets/brands/maybach.svg' },
     { name: 'new', label: 'Nouveaux modèles', icon: 'fi fi-ss-star' },
   ];
 
   private currentBrand: Brand = this.brands[0];
 
-  getBrands() {
+  public getBrands() {
     return this.brands;
   }
 
-  getCurrentBrand() {
+  public getCurrentBrand() {
     return this.currentBrand;
   }
 
-  setCurrentBrand(brand: Brand) {
+  public setCurrentBrand(brand: Brand) {
     this.currentBrand = brand;
   }
+
+  // public sanitizeBrandLogo(path: string): SafeResourceUrl {
+  //   return this.sanitizer.bypassSecurityTrustResourceUrl(
+  //     `/assets/brands/${path}`
+  //   );
+  // }
 }
